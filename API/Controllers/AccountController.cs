42c3cca71db9a3645baf3e419802014b4dbeabe9
Varun Ramesh <varunbr6@gmail.com>
2021-09-07 21:17:52 +0530

    Adding Account controller and JWT tokens

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IMapper _mapper;
+        private readonly ITokenService _tokenService;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            IMapper mapper, ITokenService tokenService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(UserRegisterDto registerDto)
+        {
+            if (await UserNameExist(registerDto.UserName))
+                return BadRequest("Username is taken.");
+
+            var user = _mapper.Map<AppUser>(registerDto);
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            result = await _userManager.AddToRoleAsync(user, "Member");
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            var token = await _tokenService.CreateToken(user);
+
+            return new UserDto
+            {
+                Name = user.Name,
+                PhotoUrl = user.Photo?.Url,
+                Token = token,
+                UserName = user.UserName
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.Users
+                .Include(u => u.Photo)
+                .FirstOrDefaultAsync(u => u.UserName == loginDto.UserName.ToLower());
+
+            if (user == null) return Unauthorized("Invalid user");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded) return Unauthorized();
+
+            var token = await _tokenService.CreateToken(user);
+
+            return new UserDto
+            {
+                Name = user.Name,
+                PhotoUrl = user.Photo.Url,
+                Token = token,
+                UserName = user.UserName
+            };
+        }
+
+        private Task<bool> UserNameExist(string userName)
+        {
+            return _userManager.Users.AnyAsync(u => u.UserName == userName.ToLower());
+        }
+    }
+}
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config, UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public async Task<string> CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.NameId,user.Id.ToString()),
+                new(JwtRegisteredClaimNames.UniqueName,user.UserName)
+            };
+            var roles = await _userManager.GetRolesAsync(user);
+
+            claims.AddRange(roles.Select(role=>new Claim(ClaimTypes.Role,role)));
+
+            var cred = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = cred,
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddMinutes(2)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
