b57103732109d1b8868a69ae6213338f140f9131
Varun Kumar BR <30699822+varunbr@users.noreply.github.com>
2021-09-12 12:57:49 +0530

    Adding bootstrap, bootswatch, font awesome to project and Home component page (#7)
    - Added bootstrap and font awesome to project.
    - Added bootswatch (bootstrap theme.)
    - Added Navbar and Home Page components.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { SharedModule } from './_modules/shared.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
