3e2a67b3e65e3c5893c3ec95ca82b5b2dfdd297c
Varun Kumar BR <30699822+varunbr@users.noreply.github.com>
2021-09-26 18:15:35 +0530

    Adding font-awesome icons (#19)

--- client/src/app/moderator/administration/administration.component.ts
+++ client/src/app/moderator/administration/administration.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
 import { AdminRole, AdminRoleParams } from 'src/app/_modals/admin';
+import { User } from 'src/app/_modals/user';
+import { AccountService } from 'src/app/_services/account.service';
 import { AdministratorService } from 'src/app/_services/administrator.service';
 import { BaseListComponent } from 'src/app/_services/base-list.component';
 
@@ -14,18 +17,23 @@ export class AdministrationComponent
   extends BaseListComponent<AdminRole, AdminRoleParams>
   implements OnInit
 {
+  currentUser: User;
   adminRole: AdminRole;
   modalRef?: BsModalRef;
   constructor(
     private administrator: AdministratorService,
     private bsmodalService: BsModalService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private accountService: AccountService
   ) {
     super(administrator);
   }
 
   ngOnInit(): void {
     this.loadModals();
+    this.accountService.user$
+      .pipe(take(1))
+      .subscribe((user) => (this.currentUser = user));
   }
 
   removeRole(adminRole: AdminRole, index: number) {
@@ -41,7 +49,6 @@ export class AdministrationComponent
   }
 
   addRole() {
-    console.log('Add');
     this.administrator.addRole(this.adminRole).subscribe((response) => {
       this.modals.push(response);
       this.toastr.success('Role added.');
